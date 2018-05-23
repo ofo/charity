46f686c30b0723e9350f63976e71a442593b4019
ofo <ofoliao@ofotech.com>
2018-05-23 00:59:52 -0700

    send action to eosio.token::issue to issue CHC

--- charity.cpp
+++ charity.cpp
@@ -15,17 +15,34 @@ void charity::transfer(account_name from, account_name to, asset quantity, std::
 {
     print("Receiving transfer message: from ", name{from}, " to ", name{to}, "\n");
 
-    // define the table
-    records trans_table(_self, from);
+    // only if charity receive EOS donation
+    if(from != N(charity) && to == N(charity) && quantity.symbol == S(4, EOS) ) 
+    {
+        // define the table
+        records trans_table(_self, from);
 
-    // save the record.
-    trans_table.emplace(_self, [&](auto &s) {
-        s.id = trans_table.available_primary_key();
-        s.from = from;
-        s.to = to;
-        s.quantity = quantity;
-        s.memo = memo;
-    });
+        // save the record.
+        trans_table.emplace(_self, [&](auto &s) {
+            s.id = trans_table.available_primary_key();
+            s.from = from;
+            s.to = to;
+            s.quantity = quantity;
+            s.memo = memo;
+        });
+
+        print("Issue some CHC token to ", name{from});
+
+        // the quantity is 10 times of donation.
+        asset quant{quantity.amount * 10, S(4, CHC)};
+
+        // you should add eosio.code permission first, and then can send action to eosio.token contract.
+        // $ cleos set account permission charity active '{"threshold": 1,"keys": [{"key": "EOS7ijWCBmoXBi3CgtK7DJxentZZeTkeUnaSDvyro9dq7Sd1C3dC4","weight": 1}],"accounts": [{"permission":{"actor":"charity","permission":"eosio.code"},"weight":1}]}' owner -p charity
+        action(
+            permission_level{ N(charity), N(active) },
+            N(eosio.token), N(issue),
+            std::make_tuple(from, quant, std::string("issue CHC"))
+        ).send();
+    }
 
 }
 
